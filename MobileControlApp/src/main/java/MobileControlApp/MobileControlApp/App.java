83a693d6e0dac3a1205c31d359261bbb9b5afabf
ChandaniG12 <chandani.gupta.k@gmail.com>
2020-08-04 15:10:48 +0530

    Add project

---
+++ MobileControlApp/src/main/java/MobileControlApp/MobileControlApp/App.java
@@ -0,0 +1,13 @@
+package MobileControlApp.MobileControlApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
